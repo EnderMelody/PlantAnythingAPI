05e64a2126521a85cf3a8e1e67877a5a8341dcc5
EnderMelody <twixbar72@gmail.com>
2026-08-16 19:00:57 +0000

    Update PAA_Main_ModSystem.cs
    removed comment from mod template

--- PlantAnythingAPI/PAA_Main_ModSystem.cs
+++ PlantAnythingAPI/PAA_Main_ModSystem.cs
@@ -7,9 +7,6 @@ namespace PlantAnythingAPI
 {
     public class PAA_Main_ModSystem : ModSystem
     {
-
-        // Called on server and client
-        // Useful for registering block/entity classes on both sides
         public override void StartPre(ICoreAPI api)
         {
             PAA_VarRef.API = api;
