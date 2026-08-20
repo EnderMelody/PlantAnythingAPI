5b0497b9e715489e6e1f529fc437e78d29f8e302
EnderMelody <twixbar72@gmail.com>
2026-08-20 00:04:34 +0000

    Update 1.1.0

--- PlantAnythingAPI/PAA_Main_ModSystem.cs
+++ PlantAnythingAPI/PAA_Main_ModSystem.cs
@@ -7,6 +7,9 @@ namespace PlantAnythingAPI
 {
     public class PAA_Main_ModSystem : ModSystem
     {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
         public override void StartPre(ICoreAPI api)
         {
             PAA_VarRef.API = api;
